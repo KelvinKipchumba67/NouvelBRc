df10e3e0b46e2b932c3fbc65ee647af2a3fe3dab
Kelvin <kelvinkipchumba935@gmail.com>
2026-01-13 23:29:30 +0300

    Added navbar

--- udem/src/App.jsx
+++ udem/src/App.jsx
@@ -1,12 +1,12 @@
 import { useState } from 'react'
 import './App.css'
 import Showcase from './components/Showcase';
-import navbar from './components/navbar';
+import Navbar from './components/navbar';
 
 function App() {
   return (
-    <div className='container'>
-      <navbar />
+    <div>
+      <Navbar />
       <Showcase />
     </div>
   );
--- udem/src/components/navbar.jsx
+++ udem/src/components/navbar.jsx
@@ -5,7 +5,15 @@ function navbar(){
     return <header>
         <main>
             <nav className="navbar">
-
+                <div className="logo">NOUVEL BRc</div>
+                <ul className="nav-links">
+                    <li> <a href="/">Home</a></li>
+                    <li><a href="/blog">Blog</a></li>
+                    <li><a href="/picks">Our Picks</a></li>
+                    <li><a href="/about">About Us</a></li>
+                    <li><a href="/contact">Contact Us</a></li>
+                    <li><a href="/join"className="join-btn">Join Us</a></li>
+                </ul>
             </nav>
         </main>
     </header>
